b6f03d74e5befe5e37e498386a7e42515ace4995
prashanthPadmashali <prashanthkumarshettigar@gmail.com>
2025-02-06 11:15:46 +0530

    Initial Commit

---
+++ Mobile-Environment/Mobile_EnvironmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mobile_EnvironmentApp.swift
+//  Mobile-Environment
+//
+//  Created by mac on 06/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Mobile_EnvironmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
